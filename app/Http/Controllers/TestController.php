eb1368283420c00480488dc719af62b02f2c6715
Yasmine <yasmineharroudi2020@gmail.com>
2026-01-07 18:23:37 +0100

    Tp Gestion routage

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+ public function index (){
+     return 'je suis le contrôleur TestController';
+    }
+
+    public function show()
+    {
+        return view('accueil');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function ()
+{ return 'Bonjour Laravel'; });
+
+Route::get('/accueil', function () {
+    return view('accueil');
+});
+
+Route::get('/test', [TestController::class, 'index']);
+Route::get('/test', [TestController::class, 'show']);
+
+Route::view('/view', 'accueil')->name('accueil');;
+Route::view('/test2', 'test');
+Route::get('/home/{nom}', function ($nom) {
+    return 'Bonjour ' . $nom;
+});
+Route::get('/home/{nom}/{age}', function ($nom, $age) {
+    return "Bonjour $nom votre Age est $age ans";
+});
+Route::get('/home3/{nom}/{age?}', function ($nom, $age = null) {
+    if ($age) {
+        return "Bonjour $nom votre Age est $age ans";
+            }
+    return "Bonjour $nom";
+});
+
