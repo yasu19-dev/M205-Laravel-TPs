d92ec8092dc88ce213ad61f6e4b8889aeac89ca9
Yasmine <yasmineharroudi2020@gmail.com>
2026-01-19 19:42:41 +0100

    first commit

---
+++ app/Http/Controllers/StagiaireController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stagiaire;
+use Illuminate\Http\Request;
+
+class StagiaireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Stagiaire $stagiaire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Stagiaire $stagiaire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Stagiaire $stagiaire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Stagiaire $stagiaire)
+    {
+        //
+    }
+}
