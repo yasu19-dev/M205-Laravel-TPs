a9468238f628fcc4c73bee837b79d4a84523f63f
Yasmine <yasmineharroudi2020@gmail.com>
2026-01-20 09:30:21 +0100

    guarded

--- app/Models/Stagiaire.php
+++ app/Models/Stagiaire.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Stmt\GroupUse;
 
 class Stagiaire extends Model
 {
     use HasFactory;
-    protected $fillable=['nom', 'prenom', 'age'];
+   // protected $fillable=['nom', 'prenom', 'age'];
+    protected $guarded = [];
 }
