5e18dc0807fcc28bb4b37cde1148acfb63a394b1
Yasmine <yasmineharroudi2020@gmail.com>
2026-01-27 11:45:20 +0100

    boucles

--- resources/views/affichage.blade.php
+++ resources/views/affichage.blade.php
@@ -27,7 +27,32 @@
             </td>
 
         </tr>
-
     </table>
+    
+    {{-- Créer une vue qui contient une liste déroulante remplie par le nombre de 0 á 9 --}}
+    <select name="" id="">
+        @for ($i=0; $i<10; $i++)
+            <option value="">{{ $i }}</option>
+        @endfor
+    </select>
+    <br>
+    {{-- Remplir une 2eme liste par les nombres impairs de 1 á 1000
+        avec deux solutions:--}}
+
+     {{-- Solution 1: avec boucle --}}
+     <select name="" id="">
+        @for ($i=1; $i<1000; $i+=2)
+            <option value="">{{ $i }}</option>
+        @endfor
+     </select>
+     <br>
+     {{-- Solution 2: avec boucle et condition --}}
+     <select name="" id="">
+        @for ($i=1; $i<1000; $i++)
+            @if ($i%2==1)
+                <option value="">{{ $i }}</option>
+            @endif
+        @endfor
+     </select>
 </body>
 </html>
