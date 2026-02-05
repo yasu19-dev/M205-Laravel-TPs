5cc481ac496a61cc30e6ce8071ff7c374130a28e
Yasmine <yasmineharroudi2020@gmail.com>
2026-02-05 13:43:10 +0100

    TP_Authentification-Laravel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -37,4 +38,25 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+
+    public function login(Request $request)
+{
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+
+        if (Auth::user()->isAdmin()) {
+            return redirect('/admin');
+        }
+
+        return redirect('/home');
+    }
+
+    return back()->withErrors([
+        'email' => 'Email ou mot de passe incorrect',
+    ]);
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+
+Route::get('/admin', function () {
+    return 'Bienvenue Admin';
+})->middleware(['auth', 'admin']);
