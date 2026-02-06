c4480619838b075350364ef6624c5bea39a87590
Yasmine <yasmineharroudi2020@gmail.com>
2026-02-06 21:28:38 +0100

    Laravel_passeport

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->post('/test-post', function (Request $request) {
+    return response()->json([
+        'status' => 'Succès',
+        'message' => 'Ton API a bien reçu les données !',
+        'user' => $request->user()->name,
+        'data_received' => $request->all()
+    ]);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
